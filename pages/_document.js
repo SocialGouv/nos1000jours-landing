c51d8a02641ef1bf7fd8829419637598ea5ded88
Eric H <erich34@gmail.com>
2022-01-12 15:23:37 +0100

    Script posthog : correction de l'insertion du script

--- pages/_document.js
+++ pages/_document.js
@@ -33,10 +33,8 @@ export default class MyDocument extends Document {
       <Html lang="fr">
         <Head>
           <link rel="shortcut icon" href="/img/favicon.png" />
-          <script>
-            {`!function(t,e){var o,n,p,r;e.SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.SV=1)}(document,window.posthog||[]);
-            posthog.init('phc_4acRsG6jxERKWVghbenbwVNhFLKP9WRm7IeQoXr7TvJ',{api_host:'https://lafabriquenumerique-posthog.cloud-ed.fr'})`}
-          </script>
+          <script dangerouslySetInnerHTML={{__html: `!function(t,e){var o,n,p,r;e.SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.SV=1)}(document,window.posthog||[]);
+            posthog.init('phc_4acRsG6jxERKWVghbenbwVNhFLKP9WRm7IeQoXr7TvJ',{api_host:'https://lafabriquenumerique-posthog.cloud-ed.fr'})`}}/>
         </Head>
         <body id="page-top">
           <Main />
